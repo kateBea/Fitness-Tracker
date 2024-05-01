c2945c7ef4ae784c4671a5983baaea50c480f734
ElGuilleDEV <guilleqdev@gmail.com>
2024-05-01 12:16:36 +0200

    Modificadas las carpetas privadas y publicas

---
+++ src/web-client/fitness-tracker-webclient/src/pages/private/AIAssistancePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AIAssistancePage() {
+  return (
+    <div>AIAssistancePage</div>
+  )
+}
+
+export default AIAssistancePage
---
+++ src/web-client/fitness-tracker-webclient/src/pages/private/MainPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+
+function MainPage() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default MainPage
