4148f91efdf23d740bab61108767220f1c8a8c54
ケイト <55351037+kateBea@users.noreply.github.com>
2024-05-05 16:44:05 +0200

    merged changes

---
+++ src/android-client/app/src/main/java/com/example/fitnesstrackerapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.fitnesstrackerapp
+
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.example.fitnesstrackerapp.navegacion.navegacion
+import com.example.fitnesstrackerapp.ui.theme.FitnessTrackerAppTheme
+
+class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val navController = rememberNavController()
+
+            FitnessTrackerAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    navegacion(navController)
+                }
+            }
+        }
+    }
+}
