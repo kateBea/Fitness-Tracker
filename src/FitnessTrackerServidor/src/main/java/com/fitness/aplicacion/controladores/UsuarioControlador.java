8d2f1b09221c9b3b540746ceddbdba9a8cafbb0c
ケイト <55351037+kateBea@users.noreply.github.com>
2024-05-18 21:28:51 +0200

    fix base url

--- src/FitnessTrackerServidor/src/main/java/com/fitness/aplicacion/controladores/UsuarioControlador.java
+++ src/FitnessTrackerServidor/src/main/java/com/fitness/aplicacion/controladores/UsuarioControlador.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fitness.aplicacion.servicio.UsuarioServicioImpl;
 
 @RestController
-@RequestMapping("Usuarios")
+@RequestMapping("api/fitnesstracker")
 public class UsuarioControlador {
 	
 	@Autowired // Inyección de dependencia del servicio de usuario
