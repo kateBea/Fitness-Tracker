6aa7e70a3fb32792480dca6bebced8c31e020fcd
ElGuilleDEV <guilleqdev@gmail.com>
2024-05-26 15:13:59 +0200

    Añadido apartado de nuestros servicios, lideres en salud y descarga app. Falta footer resto completo

--- src/web-client/fitness-tracker-webclient/src/pages/public/InicioPage.jsx
+++ src/web-client/fitness-tracker-webclient/src/pages/public/InicioPage.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {styled} from '@mui/material/styles';
 import {
   Box,
   AppBar,
@@ -6,15 +7,45 @@ import {
   Typography,
   MenuItem,
   Button,
-  Grid
+  Grid,
+  IconButton
 } from '@mui/material';
+import DownloadIcon from '@mui/icons-material/Download';
 
 import LogoFitness from '../../img/logo-fitness-tracker-oscuro.png'
 import BackgroundPuntos from '../../img/background-puntos.png'
 import RelojInicio from '../../img/reloj-inicio.png'
 import BarraSeparacion from "../../img/barra-separacion.png"
+import DecoracionServicios from "../../img/decoracion-nuestros-servicios.png"
+import BusquedaInteligente from "../../img/busqueda-inteligente.png"
+import SmartwatchIco from "../../img/smartwatch-ico.png"
+import ConsultaPersonalizada from "../../img/consulta-personalizada.png"
+import TrackingIco from "../../img/tracking-ico.png"
+import CuentaIco from "../../img/cuenta-icono.png"
+import InfoDetallada from "../../img/info-detallada.png"
+import LideresSalud from "../../img/lideres-salud.png"
+import SmartTraclerApp from "../../img/smart-tracker-app.png"
 
 function MainPage() {
+
+  const ColorButton = styled(Button)(({ theme }) => ({
+    color: '#436489',
+    borderWidth:'2px',
+    borderStyle:'solid',
+    borderRadius:'30px',
+    paddingLeft:'15px',
+    paddingRight:'15px',
+    marginTop:'20px',
+    fontWeight:'bold',
+    '&:hover': {
+      backgroundColor: '#436489',
+      color:'white',
+      '& .MuiSvgIcon-root': {
+        color: 'white',
+      }
+    },
+  }));
+
   return (
     <Box>
       <AppBar
@@ -52,7 +83,7 @@ function MainPage() {
               }}
             />
             <Typography
-              variant="h3"
+              variant="h2"
               component="h1"
               href="/"
               sx={{
@@ -110,13 +141,7 @@ function MainPage() {
             y de salud para tu beneficio personal en base a tus
             objetivos y metas</Typography>
           <Container sx={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-            <Button sx={{
-              background: '#436489',
-              fontWeight: 'bold',
-              color: 'white',
-              borderRadius: '20px',
-              right: ''
-            }}>Consigue nuestra app</Button>
+            <ColorButton>Consigue nuestra app</ColorButton>
           </Container>
         </Grid>
         <Grid item lg={6} xs={12}>
@@ -128,36 +153,237 @@ function MainPage() {
       <Box sx={{ display: 'flex', justifyContent: 'center' }}>
         <Grid container>
           <Grid item xs={12}>
-            <Box sx={{ display: 'flex', justifyContent: 'center',marginTop:'100px' }}>
+            <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '100px' }}>
               <Typography
                 variant="h3"
                 component="h2"
                 href="/"
-                sx={{fontWeight: 'bold' }}
+                sx={{ fontWeight: 'bold' }}
               >Nuestros servicios
               </Typography>
             </Box>
           </Grid>
           <Grid item xs={12}>
-            <Box sx={{ display: 'flex', justifyContent: 'center',marginTop:'50px' }}>
+            <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
               <img src={BarraSeparacion}></img>
             </Box>
           </Grid>
           <Grid item xs={12}>
-          <Box sx={{ display: 'flex', justifyContent: 'center',marginTop:'50px' }}>
+            <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
               <Typography
                 variant="p"
                 component="p"
                 href="/"
-                sx={{color: '#7D7987', maxWidth:'60%', textAlign:'center'}}
+                sx={{ color: '#7D7987', maxWidth: '60%', textAlign: 'center' }}
               >Le brindamos las mejores opciones para usted. Ajústalo a tus necesidades de salud y metas personales  Puedes consultar con nosotros qué tipo de ejercicios son mejores para alcanzar tus objetivos.
               </Typography>
             </Box>
+          </Grid>
+          <Grid item xs={12}>
+            <Box sx={{ position: 'relative', marginTop: "40px" }}>
+              <img src={DecoracionServicios} style={{ position: 'absolute', width: "900px", left: '-150px', zIndex: '0' }}></img>
+              <img src={BackgroundPuntos} style={{ position: 'absolute', width: "120px", right: '80px', top: '320px', zIndex: '0' }}></img>
+            </Box>
+            <Grid container columnSpacing={2} rowSpacing={3} sx={{ display: 'flex', justifyContent: 'center', paddingTop: '30px', paddingRight: '100px', paddingLeft: '100px' }}>
+              <Grid item xl={4} lg={4} md={6} sx={{ display: 'flex', justifyContent: 'center', }}>
+                <Box sx={{ width: '350px', height: '300px', boxShadow: '3px 10px 50px rgba(0, 0, 0, 0.2)', background: 'white', borderRadius: '20px', zIndex: '1', padding: '30px' }}>
+                  <img src={BusquedaInteligente} style={{ width: "70px" }}></img>
+                  <Typography
+                    variant="h"
+                    component="h3"
+                    href="/"
+                    sx={{ fontSize: '20pt', fontWeight: 'bold', marginTop: '30px' }}
+                  >
+                    Busqueda Inteligente
+                  </Typography>
+                  <Typography
+                    variant="p"
+                    component="p"
+                    href="/"
+                    sx={{ color: '#7D7987', marginTop: '17px' }}
+                  >Encuentra y conoce todo aquello que necesites gracias a nuestra IA interactiva acerca de la salud y el fitness
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid item xl={4} lg={4} md={6} sx={{ display: 'flex', justifyContent: 'center', }}>
+                <Box sx={{ width: '350px', height: '300px', boxShadow: '3px 10px 50px rgba(0, 0, 0, 0.2)', background: 'white', borderRadius: '20px', zIndex: '1', padding: '30px' }}>
+                  <img src={SmartwatchIco} style={{ width: "60px" }}></img>
+                  <Typography
+                    variant="h"
+                    component="h3"
+                    href="/"
+                    sx={{ fontSize: '20pt', fontWeight: 'bold', marginTop: '10px' }}
+                  >
+                    Smartwatch
+                  </Typography>
+                  <Typography
+                    variant="p"
+                    component="p"
+                    href="/"
+                    sx={{ color: '#7D7987', marginTop: '17px' }}
+                  >Escoge cualquier smartwatch y comienza a usar la app para mejorar tu salud y alcanzar tus objetivos
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid item xl={4} lg={4} md={6} sx={{ display: 'flex', justifyContent: 'center', }}>
+                <Box sx={{ width: '350px', height: '300px', boxShadow: '3px 10px 50px rgba(0, 0, 0, 0.2)', background: 'white', borderRadius: '20px', zIndex: '1', padding: '30px' }}>
+                  <img src={ConsultaPersonalizada} style={{ width: "55px" }}></img>
+                  <Typography
+                    variant="h3"
+                    component="h3"
+                    href="/"
+                    sx={{ fontSize: '18pt', fontWeight: 'bold', marginTop: '20px' }}
+                  >
+                    Consulta Personalizada
+                  </Typography>
+                  <Typography
+                    variant="p"
+                    component="p"
+                    href="/"
+                    sx={{ color: '#7D7987', marginTop: '17px' }}
+                  >Consulta gratuitamente todo aquello que necesites para alcanzar tus objetivos con nuestra IA
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid item xl={4} lg={4} md={6} sx={{ display: 'flex', justifyContent: 'center', }}>
+                <Box sx={{ width: '350px', height: '300px', boxShadow: '3px 10px 50px rgba(0, 0, 0, 0.2)', background: 'white', borderRadius: '20px', zIndex: '1', padding: '30px' }}>
+                  <img src={InfoDetallada} style={{ width: "70px" }}></img>
+                  <Typography
+                    variant="h"
+                    component="h3"
+                    href="/"
+                    sx={{ fontSize: '20pt', fontWeight: 'bold', marginTop: '30px' }}
+                  >
+                    Info Detallada
+                  </Typography>
+                  <Typography
+                    variant="p"
+                    component="p"
+                    href="/"
+                    sx={{ color: '#7D7987', marginTop: '17px' }}
+                  >
+                    Consulta la informacion acerca de tus entrenamientos y rendimiento diario
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid item xl={4} lg={4} md={6} sx={{ display: 'flex', justifyContent: 'center', }}>
+                <Box sx={{ width: '350px', height: '300px', boxShadow: '3px 10px 50px rgba(0, 0, 0, 0.2)', background: 'white', borderRadius: '20px', zIndex: '1', padding: '30px' }}>
+                  <img src={CuentaIco} style={{ width: "110px" }}></img>
+                  <Typography
+                    variant="h"
+                    component="h3"
+                    href="/"
+                    sx={{ fontSize: '20pt', fontWeight: 'bold', marginTop: '10px' }}
+                  >
+                    Cuenta
+                  </Typography>
+                  <Typography
+                    variant="p"
+                    component="p"
+                    href="/"
+                    sx={{ color: '#7D7987', marginTop: '17px' }}
+                  >
+                    Accede a tus registros desde cualquier lugar y dispositivo descargandote la app e iniciando sesion o desde nuestra web
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid item xl={4} lg={4} md={6} sx={{ display: 'flex', justifyContent: 'center', }}>
+                <Box sx={{ width: '350px', height: '300px', boxShadow: '3px 10px 50px rgba(0, 0, 0, 0.2)', background: 'white', borderRadius: '20px', zIndex: '1', padding: '30px' }}>
+                  <img src={TrackingIco} style={{ width: "80px" }}></img>
+                  <Typography
+                    variant="h"
+                    component="h3"
+                    href="/"
+                    sx={{ fontSize: '20pt', fontWeight: 'bold', marginTop: '20px' }}
+                  >
+                    Smartwatch
+                  </Typography>
+                  <Typography
+                    variant="p"
+                    component="p"
+                    href="/"
+                    sx={{ color: '#7D7987', marginTop: '20px' }}
+                  >
+                    Trackea tus entrenamientos, tu dia a dia ¡¡INCLUSO TUS PERIODOS DE SUEÑO!!
+                  </Typography>
+                </Box>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '100px' }}>
+              <img src={BarraSeparacion} style={{ width: '30%', height: '3px' }}></img>
+            </Box>
+          </Grid>
+          <Grid item xs={12} fullWidth sx={{ marginTop: '100px' }}>
+            <Grid container fullWidth>
+              <Grid item md={6} xs={12}>
+                <img src={LideresSalud} style={{ width: '95%' }}></img>
+              </Grid>
+              <Grid item md={6} xs={12}>
+                <Box sx={{ marginTop: '60px', marginLeft: '30px' }}>
+                  <Typography
+                    variant="h3"
+                    component="h3"
+                    href="/"
+                    sx={{ fontWeight: 'bold', fontSize: '30pt' }}
+                  >Lideres en salud y fitness con nuevas tecnologias
+                  </Typography>
+                  <Box sx={{ display: 'flex', justifyContent: 'start', marginTop: '30px' }}>
+                    <img src={BarraSeparacion} style={{ width: '60px', height: '3px' }}></img>
+                  </Box>
+                  <Typography
+                    variant="p"
+                    component="p"
+                    href="/"
+                    sx={{ color: '#7D7987', maxWidth: '80%', textAlign: 'left', marginTop: '30px' }}
+                  >Le brindamos las mejores opciones para usted. Ajústalo a tus necesidades de salud y metas personales  Puedes consultar con nosotros qué tipo de ejercicios son mejores para alcanzar tus objetivos.
+                  </Typography>
+                </Box>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '100px' }}>
+              <img src={BarraSeparacion} style={{ width: '30%', height: '3px' }}></img>
+            </Box>
+          </Grid>
+          <Grid item xs={12} fullWidth sx={{ marginTop: '100px' }}>
+            <Grid container fullWidth>
+
+              <Grid item md={6} xs={12}>
+                <Box sx={{ marginTop: '60px', marginLeft: '90px' }}>
+                  <Typography
+                    variant="h3"
+                    component="h3"
+                    href="/"
+                    sx={{ fontWeight: 'bold', fontSize: '30pt' }}
+                  >Descarga nuestra app para dispositivos moviles
+                  </Typography>
+                  <Box sx={{ display: 'flex', justifyContent: 'start', marginTop: '30px' }}>
+                    <img src={BarraSeparacion} style={{ width: '60px', height: '3px' }}></img>
+                  </Box>
+                  <Typography
+                    variant="p"
+                    component="p"
+                    href="/"
+                    sx={{ color: '#7D7987', maxWidth: '80%', textAlign: 'left', marginTop: '30px' }}
+                  >Nuestra aplicación dedicada a la recolección de datos a través del software del smartwatch y soluciones para salud, nutrición y rutinas fitness para el beneficio personal del usuaio. Comienza a alcanzar tus objetivos.
+                  </Typography>
+                  <ColorButton endIcon={<DownloadIcon/>}>
+                    Descargar
+                  </ColorButton>
+                  {/* sx={{color:'#436489',fontWeight:'bold',borderRadius:'30px',marginTop:'30px',borderWidth:'2px',borderColor:'#436489'}}  */}
+                </Box>
+              </Grid>
+              <Grid item md={6} xs={12}>
+                <img src={SmartTraclerApp} style={{ width: '95%', marginLeft: '30px' }}></img>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </Box>
     </Box>
   );
 }
-
 export default MainPage
