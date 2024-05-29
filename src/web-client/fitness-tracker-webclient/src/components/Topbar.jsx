54af9e569b19f1c98d9e5c5cdb26fdac346b5b2b
p-gquintanar_neoris <p-gquintanar@neoris.com>
2024-05-29 14:43:23 +0200

    Arreglado el sistema de navegacion de la topbar con navigate

--- src/web-client/fitness-tracker-webclient/src/components/Topbar.jsx
+++ src/web-client/fitness-tracker-webclient/src/components/Topbar.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { createTheme } from '@mui/material/styles';
-import { 
+import {
     Box,
     AppBar,
     Container,
@@ -12,47 +13,54 @@ import LogoFitness from '../img/logo-fitness-tracker.png';
 
 
 export const TopBar = () => {
+
+    const navigate = useNavigate();
+
+    const handleClick = (path) => {
+        navigate(path);
+    };
+
     return (
-        <AppBar 
-            position="static"  
+        <AppBar
+            position="static"
             sx={{
                 background: '#436489',
-                
+
             }}
         >
             <Container
                 maxWidth='false'
                 sx={{
-                    display:'flex',
-                    alignContent:'space-between',
-                    width:'100%',
-                    maxWidth:'100%',
-                    height:'70px'
+                    display: 'flex',
+                    alignContent: 'space-between',
+                    width: '100%',
+                    maxWidth: '100%',
+                    height: '70px'
                 }}
             >
                 <Box
                     sx={{
                         display: 'flex',
-                        alignItems:'center',
-                        width:'50%'
+                        alignItems: 'center',
+                        width: '50%'
                     }}
-                    
+
                 >
-                    <img src={LogoFitness} 
-                        title="Logo Fitness-Tracker" 
-                        alt="Logo Fitness-Tracker App" 
+                    <img src={LogoFitness}
+                        title="Logo Fitness-Tracker"
+                        alt="Logo Fitness-Tracker App"
                         style={{
-                            width:'55px',
-                            height:'55px',
+                            width: '55px',
+                            height: '55px',
                             position: 'block',
                         }}
                     />
-                    <Typography 
+                    <Typography
                         variant="h3"
                         component="h1"
                         href="/"
                         sx={{
-                            color:'white',
+                            color: 'white',
                             textDecoration: 'none',
                             margin: '15px'
                         }}
@@ -60,19 +68,19 @@ export const TopBar = () => {
                         Fitness-Tracker
                     </Typography>
                 </Box>
-                <Box 
+                <Box
                     sx={{
-                        display:'flex',
+                        display: 'flex',
                         justifyContent: 'flex-end',
-                        alignContent:'right',
-                        alignItems:'center',
-                        width:'50%',
+                        alignContent: 'right',
+                        alignItems: 'center',
+                        width: '50%',
                     }}
                 >
-                    <MenuItem href='/'>Home</MenuItem>
-                    <MenuItem href='/'>App</MenuItem>
-                    <MenuItem href='/'>Sobre Nosotros</MenuItem>
-                    <MenuItem href='/'>Nuestros Clientes</MenuItem>
+                    <MenuItem onClick={() => handleClick('/')}>Home</MenuItem>
+                    <MenuItem onClick={() => handleClick('/')}>App</MenuItem>
+                    <MenuItem onClick={() => handleClick('/')}>Sobre Nosotros</MenuItem>
+                    <MenuItem onClick={() => handleClick('/')}>Nuestros Clientes</MenuItem>
                 </Box>
             </Container>
         </AppBar>
